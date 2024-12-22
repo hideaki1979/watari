46b87859319ed6f7cae4584f18ae6de85098659f
hideaki1979 <syumeikyo@outlook.jp>
2024-12-22 21:51:06 +0900

    アカウント管理機能着手

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="w-full h-16 bg-white py-4 flex justify-center items-center gap-4 border-t">
+    <!-- メイン画面へのリンク -->
+    <a href="#" class="text-gray-500 hover:text-gray-800 w-6 h-6">
+        <i class="fa-solid fa-house"></i>
+    </a>
+    <!-- アカウント管理画面へのリンク -->
+    <a href="{{ route('account') }}" class="text-gray-500 hover:text-gray-800 w-6 h-6">
+        <i class="fa-regular fa-circle-user"></i>
+    </a>
+</div>
